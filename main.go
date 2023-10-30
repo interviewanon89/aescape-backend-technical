7ea57c6880b57c17ef25b5f07e5a31f8265510fe
anon <137334762+interviewanon89@users.noreply.github.com>
2023-10-30 13:48:24 -0600

    Create main.go

---
+++ main.go
@@ -0,0 +1,18 @@
+ /*
+# Write a function with the signature KClosest(points [][]int, k int) [][]int
+
+#where points[i] = [xi, yi] represent a point on an X-Y plane, return the k closest points to the origin (0, 0).
+
+#The distance between two points on the X-Y plane is the Euclidean distance (i.e., √((x1 - x2)^2 + (y1 - y2)^2).
+
+#You may return the answer in any order. The answer is guaranteed to be unique (except for the order that it is in).
+
+#Input: points = [][]int{{1,3},{-2,2}}, k = 1 Output: [[-2 2]] Explanation: The distance between (1, 3) and the origin is sqrt(10). The distance between (-2, 2) and the origin is sqrt(8). Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin. We only want the closest k = 1 points from the origin, so the answer is just [[-2 2]].
+
+#Example 2:
+
+#Input: points = [][]int{{3,3},{5,-1},{-2,4}}, k = 2 Output: [[3 3] [-2 4]] Explanation: The answer [[-2 4] [3 3]] is also ok.
+*/
+
+func main() {
+}
